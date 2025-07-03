fb3b6e6e74fed504ff59322a73e28852e0f0706d
Nicolas <nico.yico@gmail.com>
2025-07-03 12:58:34 -0400

    pikapika

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import PokemonFetcher from "./PokemonFetcher";
+
+function App() {
+
+  return (
+    <>
+      <h1>¡Conoce a tus Pókemons!</h1>
+      <PokemonFetcher />
+    </>
+  )
+}
+
+export default App;
